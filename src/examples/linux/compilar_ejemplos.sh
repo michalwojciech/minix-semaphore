99c15077e98ba94954d5a422b95082df0d5640f5
pabloggarcia <pabloggarcia@0b14adcf-8e22-0410-a71e-a18b474b7902>
2006-11-29 22:06:50 +0000

    Commit después del coloquio :D

--- src/examples/linux/compilar_ejemplos.sh
+++ src/examples/linux/compilar_ejemplos.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 rm -f prod_cons
-rm -f prod_cons_init
+rm -f pc_init
 rm -f barbero
 rm -f sem_serv
 gcc -g -o prod_cons ../prod_cons.c ../../fs/list.c timeutils.c sem_cli.c protocolo_sem.c libconnection.a -I ../../fs/ -I ../../libsem/ -D_LINUX__
-gcc -g -o prod_cons_init ../prod_cons_init.c ../../fs/list.c timeutils.c sem_cli.c protocolo_sem.c libconnection.a -I ../../fs/ -I ../../libsem/ -D_LINUX__
+gcc -g -o pc_init ../pc_init.c ../../fs/list.c timeutils.c sem_cli.c protocolo_sem.c libconnection.a -I ../../fs/ -I ../../libsem/ -D_LINUX__
 gcc -g -o console ../console.c ../../fs/list.c timeutils.c sem_cli.c protocolo_sem.c libconnection.a -I ../../fs/ -I ../../libsem/ -D_LINUX__
 gcc -g -o sem_serv sem_serv.c ../../fs/list.c ../../fs/hash.c timeutils.c protocolo_sem.c libconnection.a ../../fs/semaphore.c -I ../../fs/ -I ../../libsem/ -lm -D_LINUX__
